ae3239baa6d280c21cd6638ad28a81feaf950744
Oreoluwa <samuel.ajiboye456@gmail.com>
2025-10-30 00:28:57 -0400

    btn updated

--- src/components/btn.jsx
+++ src/components/btn.jsx
@@ -6,9 +6,9 @@ function IncDec(){
     return(
         <>
         <h1>{count}</h1>
-        <button className='btn' onClick={() => setCount(count + 1)}>Increase</button>
+        <button className='btn' onClick={(prevCount) => setCount(prevCount + 1)}>Increase</button>
         <button className='btn1' onClick={() => setCount(0)}>Reset</button>
-        {count == 0 ? <button className='btn2' onClick={() => setCount(count)}>Decrease</button> : <button className='btn2' onClick={() => setCount(count - 1)}>Decrease</button>}
+        {count == 0 ? <button className='btn2' onClick={() => setCount(count)}>Decrease</button> : <button className='btn2' onClick={(prevCount) => setCount(prevCount - 1)}>Decrease</button>}
 
         </>
     )
